cf75d49deb29877aa9e1c410cb5d4be6403331f9
Alchemy5 <varunhpr5@gmail.com>
2023-01-24 23:14:19 -0500

    deleted old python scripts

--- main.py
+++
@@ -1,9 +0,0 @@
-from structs import Parser
-parser = Parser("whisper")
-#parser.eval_dataset("dataset/dataset1", "output/whisper_output_1.jpg")
-#parser.eval_dataset("dataset/dataset2", "output/whisper_output_2.jpg")
-#parser.eval_dataset("dataset/dataset3", "output/whisper_output_3.jpg")
-parser.eval_dataset("dataset/dataset1", "output/whisper_output_1.jpg")
-parser.eval_dataset("dataset/dataset2", "output/whisper_output_2.jpg")
-parser.eval_dataset("dataset/dataset3", "output/whisper_output_3.jpg")
-import pdb;pdb.set_trace()
